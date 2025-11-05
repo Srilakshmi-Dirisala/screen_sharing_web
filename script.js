dbc71dba8574df30c7072a288f8fee0def187d7b
srilakshmi415 <srilakshmi@innotrat.in>
2025-11-05 23:15:18 +0530

    latest5

--- script.js
+++ script.js
@@ -908,19 +908,32 @@ this.peerConnectionConfig = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        // ✅ ADD TURN SERVERS (same as viewer.js)
         {
             urls: [
                 'turn:openrelay.metered.ca:80',
                 'turn:openrelay.metered.ca:443',
+                'turn:openrelay.metered.ca:443?transport=tcp',
                 'turns:openrelay.metered.ca:443?transport=tcp'
             ],
             username: 'openrelayproject',
             credential: 'openrelayproject'
+        },
+        {
+            urls: [
+                'turn:numb.viagenie.ca:3478?transport=udp',
+                'turn:numb.viagenie.ca:3478?transport=tcp'
+            ],
+            username: 'webrtc@live.com',
+            credential: 'muazkh'
         }
     ],
     iceTransportPolicy: 'all',
     bundlePolicy: 'max-bundle',
-    rtcpMuxPolicy: 'require'
+    rtcpMuxPolicy: 'require',
+    iceCandidatePoolSize: 10
 };
         this.initializeElements();
         this.bindEvents();

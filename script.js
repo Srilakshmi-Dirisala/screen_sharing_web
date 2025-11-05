899947bb87d57fb286e24c796586fcac757bea7c
srilakshmi415 <srilakshmi@innotrat.in>
2025-11-05 23:01:37 +0530

    latest4

--- script.js
+++ script.js
@@ -894,15 +894,34 @@ class ScreenShareApp {
         this.peerId = 'broadcaster_' + Math.random().toString(36).substr(2, 9);
         this.isPaused = false;
         
-        this.peerConnectionConfig = {
-            iceServers: [
-                { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun1.l.google.com:19302' },
-                { urls: 'stun:stun2.l.google.com:19302' },
-                { urls: 'stun:stun3.l.google.com:19302' }
-            ]
-        };
+        // this.peerConnectionConfig = {
+        //     iceServers: [
+        //         { urls: 'stun:stun.l.google.com:19302' },
+        //         { urls: 'stun:stun1.l.google.com:19302' },
+        //         { urls: 'stun:stun2.l.google.com:19302' },
+        //         { urls: 'stun:stun3.l.google.com:19302' }
+        //     ]
+        // };
         
+       // Enhanced WebRTC configuration with TURN servers
+this.peerConnectionConfig = {
+    iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun1.l.google.com:19302' },
+        {
+            urls: [
+                'turn:openrelay.metered.ca:80',
+                'turn:openrelay.metered.ca:443',
+                'turns:openrelay.metered.ca:443?transport=tcp'
+            ],
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
+    ],
+    iceTransportPolicy: 'all',
+    bundlePolicy: 'max-bundle',
+    rtcpMuxPolicy: 'require'
+};
         this.initializeElements();
         this.bindEvents();
         this.updateUI();
